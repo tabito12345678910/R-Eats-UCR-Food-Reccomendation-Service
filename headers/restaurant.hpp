25bfa275d76fc6b14bce302372d2bcc371eac52b
Sped32DJ <dannycool10236@gmail.com>
2024-02-20 17:51:09 -0800

    Looks good so far, (doesn't compile)

--- headers/restaurant.hpp
+++ headers/restaurant.hpp
@@ -1,21 +1,40 @@
+#include <iomanip>
 #include <iostream>
+#include <string>
 #include <vector>
 
-enum class MenuChoice { Chicken, Steak, Vegetarian, Vegan };
+enum class MenuChoice {
+  Chicken,
+  Beef,
+  Pork,
+  Vegetarian,
+  Vegan,
+  Mushrooms,
+  Fries,
+  Rice,
+  Beans,
+  Shrimp,
+  Vegstables
+};
 
 enum class Nationality { Japanese, Chinese, American, Halal, Mexican };
 
 enum class Location {
-  Hub,
+  HUB,
   UniversityVillage,
   Glenmore,
-  BetweenOrbachAndCollegeOfMedicine
+  BetweenOrbachAndCollegeOfMedicine,
+  Barn
 };
+
+enum class Beverages { Coffee, Tea, SodaFountain };
 
 // virtual class
 class Restaurant {
 public:
   virtual ~Restaurant() {}
+
+  virtual greeter() {} // plaeholder
 
   virtual void addMenuChoice(MenuChoice choice) {
     MenuChoice.push_back(choice);
@@ -29,9 +48,29 @@ protected:
   // These are vectors as of now, probably change it to strings or plain enums
   // This visually makes the code look clean
   std::vector<MenuChoice> vChoices;
+  std::vector<Beverages> vDrinks;
   std::vector<Nationality> vNation;
   std::vector<Location> vLocation;
+  std::string restaurant_name;
+  std::vector<std::string, std::double, std::int>
+      vMenu; // name, price, calories
   /* Location vLocation; // plain enum, keeping the v for namesake */
+};
+
+// For takeout restaurants like PandaExpress / Hibachi
+class buffetRestaurant : public Restaurant {
+  void greeter() {
+    // Will display the dollar sign values for
+    std::cout << name << " Menu\n"
+              << std::fixed << std::showpoint << std::setprecision(2)
+              << "Prices: Small(1 entree & 1 side): $" << priceS
+              << " Medium (2 entree & 1 side): $" << priceM
+              << "\nLarge(3 entree & 1 side): $" << priceL << endl;
+  }
+
+protected:
+  std::double priceS, priceM, priceL;
+  /* std::vector < */
 };
 
 // NOTE  testing purposes
@@ -41,6 +80,22 @@ public:
     // You can hard code more values since it is a vector
     vChoices.push_Back(MenuChoice::Steak);
     vNation.push_back(Nationality::American);
-    vLocation.push_back(Location::Hub);
+    vLocation.push_back(Location::HUB);
+  }
+};
+
+class PandaExpress : public buffetRestaurant {
+  PandaExpress() {
+    priceS = 8.40;
+    priceM = 9.90;
+    princeL = 11.40;
+    vLocation.push_back(Location::HUB);
+    vDrinks.push_back(Beverages::SodaFountain);
+    vNation.push_back(Nationality::Chinese);
+    vMenu.push_back(MenuChoice::Chicken);
+    vMenu.push_back(MenuChoice::Beef);
+    vMenu.push_back(MenuChoice::Rice);
+    vMenu.push_back(MenuChoice::Shrimp);
+    vMenu.push_back(MenuChoice::Vegstables);
   }
 };
