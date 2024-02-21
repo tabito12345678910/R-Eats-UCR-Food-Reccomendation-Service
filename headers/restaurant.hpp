8eac56c94e743743917a5ece17edbaba521e2397
Sped32DJ <dannycool10236@gmail.com>
2024-02-20 18:16:52 -0800

    Adding Panda to it's own file!

--- headers/restaurant.hpp
+++ headers/restaurant.hpp
@@ -83,19 +83,3 @@ public:
     vLocation.push_back(Location::HUB);
   }
 };
-
-class PandaExpress : public buffetRestaurant {
-  PandaExpress() {
-    priceS = 8.40;
-    priceM = 9.90;
-    princeL = 11.40;
-    vLocation.push_back(Location::HUB);
-    vDrinks.push_back(Beverages::SodaFountain);
-    vNation.push_back(Nationality::Chinese);
-    vMenu.push_back(MenuChoice::Chicken);
-    vMenu.push_back(MenuChoice::Beef);
-    vMenu.push_back(MenuChoice::Rice);
-    vMenu.push_back(MenuChoice::Shrimp);
-    vMenu.push_back(MenuChoice::Vegstables);
-  }
-};
