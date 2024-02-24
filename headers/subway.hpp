d476273464c42ea8f765fe6791e3974b60b8e22a
tabito12345678910 <tabito.sakamoto@gmail.com>
2024-02-23 21:14:51 -0800

    fixed subway

--- headers/subway.hpp
+++ headers/subway.hpp
@@ -1,3 +1,21 @@
 #include "restaurant.hpp"
+class Subway : public Restaurant {
+  Subway() {
+    meal = "Sub sandwiches";
+    priceSDrink = 2.59;
+    priceMDrink = 2.99;
+    priceLDrink = 3.39;
+    vLocation.push_back(Location::HUB);
+    vDrinks.push_back(Beverages::SodaFountain);
+    vNation.push_back(Nationality::American);
 
+    vMenu.push_back(MenuChoice::Beef);
+    vMenu.push_back(MenuChoice::Sandwich);
+    vMenu.push_back(MenuChoice::Chicken);
+    vMenu.push_back(MenuChoice::Turkey);
+    vMenu.push_back(MenuChoice::Vegetables);
+    vMenu.push_back(MenuChoice::Dessert)
+    vMenu.push_back(MenuChoice::Vegetarian);
+  }
+};
 
