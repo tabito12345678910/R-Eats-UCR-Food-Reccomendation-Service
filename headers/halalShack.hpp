618b2b4d8a57d96892c968d5a2383fe11efd7db4
tabito12345678910 <tabito.sakamoto@gmail.com>
2024-02-23 18:43:33 -0800

    finished making .hpp files

--- headers/halalShack.hpp
+++ headers/halalShack.hpp
@@ -1,1 +1,18 @@
 #include "restaurant.hpp"
+
+class HalalShack : public Restaurant {
+  HalalShack() {
+    meal = "Mediterranean street food";
+    priceMDrink = 2.59;
+    vLocation.push_back(Location::HUB);
+    vDrinks.push_back(Beverages::SodaFountain);
+    vNation.push_back(Nationality::Halal);
+
+    vMenu.push_back(MenuChoice::Fries);
+    vMenu.push_back(MenuChoice::Beef);
+    vMenu.push_back(MenuChoice::Rice);
+    vMenu.push_back(MenuChoice::Chicken);
+    vMenu.push_back(MenuChoice::Vegetables);
+    vMenu.push_back(MenuChoice::Vegetarian);
+  }
+};
