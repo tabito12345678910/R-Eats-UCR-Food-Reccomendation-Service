72f08ec32b79359808a9e45f1304563637ec3580
idkmori <ryanrobes934@icloud.com>
2024-02-26 17:57:06 -0800

    changed options to strings, fixed typos

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,39 @@ using std::cout;
 using std::endl;
 using std::string;
 
-void meatOptions() { //displays meat options when "Meat" is selected
+string meatOptions() { //displays meat options when "Meat" is selected
+  string choice;
   cout << "Meat Options" << endl
        << "1. Beef" << endl
        << "2. Chicken" << endl
        << "3. Pork" << endl
        << "4. Turkey" << endl
-       << "5. [Back]" << endl; //"Back" to see other food preferences 
+       << "5. [Back]" << endl; //"Back" to go back to food preferences 
+  
+  cin >> choice;
+  cin.clear();
+  cin.ignore(256, '\n');
+  return choice;
 }
 
-void seafoodOptions() {
+string seafoodOptions() { //displays meat options when "Seafood" is selected
+  string choice;
   cout << "Seafood Options" << endl
        << "1. Crab" << endl
        << "2. Salmon" << endl
        << "3. Scallops" << endl
        << "4. Shrimp" << endl
        << "5. Tuna" << endl
-       << "6. [Back]" << endl;
+       << "6. [Back]" << endl; //"Back" to go back to food preferences
+  
+  cin >> choice;
+  cin.clear();
+  cin.ignore(256, '\n');
+  return choice;
 }
 
-void dishOptions() {
+string dishOptions() { //displays meat options when "Dishes" is selected
+  string choice;
   cout << "Dishes Options" << endl
        << "1. Burger" << endl
        << "2. Burritos" << endl
@@ -36,17 +49,28 @@ void dishOptions() {
        << "6. Salad" << endl
        << "7. Sandwich" << endl
        << "8. Tacos" << endl
-       << "9. [Back]" << endl;
+       << "9. [Back]" << endl; //"Back" to go back to food preferences
+  
+  cin >> choice;
+  cin.clear();
+  cin.ignore(256, '\n');
+  return choice;
 }
 
-void cuisineOptions() {
+string cuisineOptions() { //displays meat options when "Cuisine" is selected
+  string choice;
   cout << "Cuisine Options:" << endl
        << "1. American" << endl
        << "2. Chinese" << endl
        << "3. Japanese" << endl
        << "4. Mediterranean" << endl
        << "5. Mexican" << endl
-       << "6. [Back]" << endl;
+       << "6. [Back]" << endl; //"Back" to go back to food preferences
+
+  cin >> choice;
+  cin.clear();
+  cin.ignore(256, '\n');
+  return choice;
 }
 
 string menu() {
@@ -61,10 +85,10 @@ string menu() {
        << "3. Vegetarian" << endl
        << "4. Vegan" << endl
        << "5. Vegetables" << endl
-       << "6. Dishs:" << endl
+       << "6. Dishes:" << endl
        << "7. Dessert" << endl
        << "8. Cuisines:" << endl
-       << "9. No preference" << endl; // default
+       << "9. No preference" << endl; // default; will give every and all options
   cin >> choice;
 
   // fix buffer just in case non-numeric choice entered
