92c7c7e2c914bcfe7de89f7f212623b2703fcb8f
“nathan0404” <nmats016@ucr.edu>
2024-02-28 15:36:06 -0800

    added basic constructor tests, not sure what else to add

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,34 @@
 #include "../headers/restaurants.hpp"
 #include "gtest/gtest.h"
 
-TEST(RestaurantTests, testingGreeter) {
-    
+TEST(RestaurantTestsChronicTacos, ChronicTacosConstructor) {
+    EXPECT_NO_THROW({ChronicTacos testChronicTacos();});
+}
+
+TEST(RestaurantTestsCoffeeBean, CoffeeBeanConstructor) {
+    EXPECT_NO_THROW({CoffeeBean testCoffeeBean();});
+}
+
+TEST(RestaurantTestsHabit, HabitConstructor) {
+    EXPECT_NO_THROW({Habit testHabit();});
+}
+
+TEST(RestaurantTestsHalalShack, HalalShackConstructor) {
+    EXPECT_NO_THROW({HalalShack testHalalShack();});
+}
+
+TEST(RestaurantTestsHibachi, HibachiConstructor) {
+    EXPECT_NO_THROW({Hibachi testHibachi();});
+}
+
+TEST(RestaurantTestsPandaExpress, PandaExpressConstructor) {
+    EXPECT_NO_THROW({PandaExpress testPandaExpress();});
+}
+
+TEST(RestaurantTestsStarbucks, StarbucksConstructor) {
+    EXPECT_NO_THROW({Starbucks testStarbucks();});
+}
+
+TEST(RestaurantTestsSubway, SubwayConstructor) {
+    EXPECT_NO_THROW({Subway testSubway();});
 }
