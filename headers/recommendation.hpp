3afe1e6a8c871eda96db0ccbcc757c8d396b58f1
Sped32DJ <dannycool10236@gmail.com>
2024-03-08 19:54:33 -0800

    Formatting for readability

--- headers/recommendation.hpp
+++ headers/recommendation.hpp
@@ -1,33 +1,31 @@
 #ifndef RECOMMENDATION_HPP
 #define RECOMMENDATION_HPP
 #include "restaurants.hpp"
-#include "vRestaurants.hpp" 
+#include "vRestaurants.hpp"
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 
-class UserPreference { 
-    private:
-        //user preferences
-        MenuChoice pref1;
-        MenuChoice pref2;
-        MenuChoice pref3;
-        //check if campus has meal that matches preference
-        bool includesMeal = false;
-    public:
-        //constructor
-        UserPreference(MenuChoice p1, MenuChoice p2, MenuChoice p3): pref1(p1), pref2(p2), pref3(p3){}
-        //get preference
-        MenuChoice getPref1(){return pref1;}
-        MenuChoice getPref2(){return pref2;}
-        MenuChoice getPref3(){return pref3;}
-        vector<Restaurant> RestaurantsThatIncludePreferences(){};
-        void printMeals(vector<Restaurant>){};
+class UserPreference {
+private:
+  // user preferences
+  MenuChoice pref1;
+  MenuChoice pref2;
+  MenuChoice pref3;
+  // check if campus has meal that matches preference
+  bool includesMeal = false;
 
+public:
+  // constructor
+  UserPreference(MenuChoice p1, MenuChoice p2, MenuChoice p3)
+      : pref1(p1), pref2(p2), pref3(p3) {}
+  // get preference
+  MenuChoice getPref1() { return pref1; }
+  MenuChoice getPref2() { return pref2; }
+  MenuChoice getPref3() { return pref3; }
+  vector<Restaurant> RestaurantsThatIncludePreferences(){};
+  void printMeals(vector<Restaurant>){};
 };
 
-
-
 #endif
-
--- src/recommendation.cpp
+++ src/recommendation.cpp
@@ -1,40 +1,31 @@
 #include "../headers/recommendation.hpp"
 
+vector<Restaurant> UserPreference::RestaurantsThatIncludePreferences() {
+  vectorRestaurants restaurants;
+  vector<Restaurant> vIncludesPreference;
+  for (int i = 0; i < restaurants.getRestaurants().size(); ++i) {
+    int count = 0;
+    for (int j = 0; j < restaurants.getRestaurant(i).getMenu().size(); ++j) {
+      if (restaurants.getRestaurant(i).getMenu().at(j) == pref1) {
+        count = count + 1;
+      } else if (restaurants.getRestaurant(i).getMenu().at(j) == pref2) {
+        count = count + 1;
+      } else if (restaurants.getRestaurant(i).getMenu().at(j) == pref3) {
+        count = count + 1;
+      }
+    }
+    if (count == 3) {
+      vIncludesPreference.push_back(restaurants.getRestaurant(i));
+      includesMeal = true;
+    }
+  }
 
-vector<Restaurant> UserPreference::RestaurantsThatIncludePreferences(){
-    vectorRestaurants restaurants;
-    vector<Restaurant> vIncludesPreference;
-        for(int i = 0; i<restaurants.getRestaurants().size(); ++i){
-            int count = 0;
-            for(int j = 0; j<restaurants.getRestaurant(i).getMenu().size(); ++j){
-                if(restaurants.getRestaurant(i).getMenu().at(j)==pref1){
-                    count = count + 1;
-                }
-                else if(restaurants.getRestaurant(i).getMenu().at(j)==pref2){
-                    count = count + 1;
-                }
-                else if(restaurants.getRestaurant(i).getMenu().at(j)==pref3){
-                    count = count + 1;
-                }
-
-            }
-            if(count == 3){
-                vIncludesPreference.push_back(restaurants.getRestaurant(i));
-                includesMeal = true;
-            }
-        }
-
-    return vIncludesPreference;
+  return vIncludesPreference;
 }
 
-
-void UserPreference::printMeals(vector<Restaurant> vIncludesPref){
-    if(includesMeal == false){
-        cout<<"No Restaurant on Campus has meals that match your "<<endl;
-    }
-    else{
-        
-    }
+void UserPreference::printMeals(vector<Restaurant> vIncludesPref) {
+  if (includesMeal == false) {
+    cout << "No Restaurant on Campus has meals that match your " << endl;
+  } else {
+  }
 }
-
-
