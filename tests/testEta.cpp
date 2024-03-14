8f5177268500169cfafbcc7b7203246c38d7223d
“nathan0404” <nmats016@ucr.edu>
2024-03-13 21:06:40 -0700

    testing mocktesteta

--- tests/testEta.cpp
+++ tests/testEta.cpp
@@ -13,10 +13,10 @@ TEST(MockGetEtaTest, testmockgetEta) {
     // Setup
     Eta mockEta;
     // Test cases
-    EXPECT_EQ(mockEta.getEta("Hub", Location::HUB), 0);
-    EXPECT_EQ(mockEta.getEta("MSE", Location::HUB), 5);
-    EXPECT_EQ(mockEta.getEta("BigSprings", Location::Glenmore), 5);
-    EXPECT_EQ(mockEta.getEta("Sproul", Location::MSE), 13);
-    EXPECT_EQ(mockEta.getEta("EntemologyMuseum", Location::MSE), -1);
-    EXPECT_NE(mockEta.getEta("Hub", Location::HUB), 10);
+    EXPECT_EQ(mockEta.getEta("hub", Location::HUB), 0);
+    EXPECT_EQ(mockEta.getEta("mse", Location::HUB), 5);
+    EXPECT_EQ(mockEta.getEta("bigsprings", Location::Glenmore), 5);
+    EXPECT_EQ(mockEta.getEta("sproul", Location::MSE), 13);
+    EXPECT_EQ(mockEta.getEta("entemologymuseum", Location::MSE), -1);
+    EXPECT_NE(mockEta.getEta("hub", Location::HUB), 10);
 }
